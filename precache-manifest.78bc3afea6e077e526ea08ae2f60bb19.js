b3c54ce7563dd3a3f9feab136b8251b3d14f72f7
jarri <jarri@Metsanpeitto.com>
2020-09-12 18:49:19 +0200

    Updates

---
+++ precache-manifest.78bc3afea6e077e526ea08ae2f60bb19.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4cbbb1e459dc7af260a104f6302c2d9",
+    "url": "/react-color-picker/index.html"
+  },
+  {
+    "revision": "87f3573c8e9456760132",
+    "url": "/react-color-picker/static/css/main.1b5ee615.chunk.css"
+  },
+  {
+    "revision": "de922e407cafd9c5f224",
+    "url": "/react-color-picker/static/js/2.521049c6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-color-picker/static/js/2.521049c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87f3573c8e9456760132",
+    "url": "/react-color-picker/static/js/main.157e2019.chunk.js"
+  },
+  {
+    "revision": "e12d0f17b117823751a5",
+    "url": "/react-color-picker/static/js/runtime-main.103c7576.js"
+  }
+]);
